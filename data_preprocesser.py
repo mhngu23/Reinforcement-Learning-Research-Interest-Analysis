feb7ecc8f959e808aa3ca5f5f1781e443085d6e0
mhngu23 <minhhoangnguyen0911@gmail.com>
2023-02-18 18:31:55 +1100

    add data_preprocesser and testing clusterinig

---
+++ data_preprocesser.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import os
+import glob
+import warnings
+warnings.filterwarnings("ignore")
+
+import re
+
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from k_mean_clustering import k_mean_clustering
+
+PATH = os.getcwd() + "\\data\\"
+
+def csv_data_into_df_reader():
+    """
+    This function is used to read all csv files from supplied path
+    """
+    csv_files = glob.glob(os.path.join(PATH, "*.csv"))
+    
+    df = pd.DataFrame()
+    for f in csv_files:
+        csv = pd.read_csv(f, index_col=0)
+        df = df.append(csv)
+    
+    return df   
+
+
+def text_preprocessor(sentence):
+    """
+    This function is used to process the input sentence
+    :param sentence: (str) The sentence to be processed
+    """
+    wpt = nltk.WordPunctTokenizer()
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+
+    sentence = sentence.lower() # Convert text to lower case
+
+    sentence = re.sub('[^A-Za-z0-9]+', ' ', sentence) # Remove special characters
+
+    sentence = ' '.join(char for char in sentence.split() if char not in stop_words)  # Remove un-needed stopwords
+
+    sentence = re.sub(r"'<.*?>'", "", sentence) # Remove tag
+
+    sentence = " ".join([lemmatizer.lemmatize(char) for char in sentence.split()]) # Apply lemmatization
+
+    return sentence
+
+
+def dataframe_preprocessor(df):
+    """
+    This function is used to preprocessed raw dataframe
+    """
+
+    index_list = list(range(1, df.shape[0]+1))
+    df.index = index_list
+
+    df = df.drop_duplicates(subset=['title'], keep='first')
+    print(df)
+    df['processed_title'] = df['title'].apply(lambda x: text_preprocessor(x))
+
+    return df
+
+if __name__=="__main__":
+
+    
+    raw_df = csv_data_into_df_reader()
+
+    preprocessed_df = dataframe_preprocessor(raw_df)
+
+    print("----------------------------------------------------------")
+    print(f"The number of articles are {str(preprocessed_df.shape[0])}")
+    print("----------------------------------------------------------")
+
+
+    a = 0
+
+    while str(a) != "a":
+        a = input("Press a to continue: ")
+
+    k_mean_clustering(preprocessed_df)
+    
